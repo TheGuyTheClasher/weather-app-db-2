b41e6a8a42edd4e1c5f15d6750fc8597f8d14819
“Chinmay <chinmayhardikar@bulbandkey.com>
2021-02-07 00:00:14 +0530

    Response handling for weather data

--- app.js
+++ app.js
@@ -14,15 +14,38 @@ let port = process.env.PORT || 4000;
 
 app.get('/getweather', cors(), (req, res) => {
     console.log(req.query);
-    request(`http://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=1a519892f1765781dca5cbc681371375`)
-        .on('data', (chunk) => {
-            let obj_data = JSON.parse(chunk);
-            res.send(obj_data);
-        })
-        .on('end', (err) => {
-            if (err) return console.log('connection closed due to errors', err);
-            console.log('end');
-        });
+    try {
+      request(`http://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=1a519892f1765781dca5cbc681371375`)
+          .on('data', (chunk) => {
+              let obj_data = JSON.parse(chunk);
+
+              if(obj_data['cod'] == 200){
+
+                let output_obj = {
+                  'status' : 'success',
+                  'weather_data': obj_data,
+                  'message': "Weather data fetched successfully!!!"
+                }
+                res.send(output_obj);
+              } else {
+                let output_obj = {
+                  'status' : 'failure',
+                  'weather_data': obj_data,
+                  'message': `Unable to fetch data for ${req.query.name}`
+                }
+                res.send(output_obj);
+              }
+
+          })
+          .on('end', (err) => {
+              if (err) return console.log('connection closed due to errors', err);
+              console.log('end');
+          });
+    } catch (e) {
+      res.send(e);
+    }
+
+
 });
 
 app.listen(port, (err, success) => {
